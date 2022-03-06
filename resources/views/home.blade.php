4449c2cb36fca827a727ca238a6cd2f4a1fb5b55
Super Visor <supervisor@zibaie2404.local>
2022-03-06 17:23:03 +0330

    design resume form wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Jobinja Resume Builder</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-+qdLaIRZfNu4cVPK/PxJJEy0B0f3Ugv8i482AKY7gwXwhaCroABd086ybrVKTa0q" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('assets/style.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light " id="menu">
             <div class="container-fluid">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -21,6 +24,10 @@
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">خانه</a>
+                            
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">رزومه های من</a>
 
                         </li>
                     </ul>
@@ -40,7 +47,79 @@
 
     </header>
 
+    <div class="container mt-5">
+        <div class="row">
 
+            <div class="col-lg-8">
+                {{-- Resume section --}}
+                <div class="row summary">
+                    <div class="col-2 text-center">
+                        <div class="picture-icon">
+                            <i class="fa fa-solid fa-user"></i>
+
+                        </div>
+                        <a class="btn btn-light mt-1 w-100 upload-image-btn" href="#">آپلود عکس</a>
+                    </div>
+                    <div class="col-10">
+
+                        <div class=" w-100">
+                            <ul>
+                                <li class="fullname">
+                                    رضا سلام محمره
+                                </li>
+                                <li>
+
+                                    عنوان شغلی:برنامه نویس
+
+
+                                </li>
+                                <li>
+                                    وضعیت اشتغال:شاغل
+                                </li>
+                                <li>
+                                    آخرین شرکت:ایران خودرو زیبایی
+                                </li>
+                                <li>
+                                    آخرین مدرک تحصیلی:کارشناسی ارشد
+                                </li>
+                            </ul>
+
+                            <a class="btn btn-success" href="#">مشاهده رزومه</a>
+                            <a class="download-cv-btn p-1" href="#">دریافت فایل رزومه</a>
+                        </div>
+
+
+
+                    </div>
+                </div>
+                {{-- End Resume section --}}
+                
+                {{-- Resume Section --}}
+                <div class="row personal-info">
+                    <div class="box-header">
+
+                    </div>
+                </div>
+                {{-- End Resume section --}}
+               
+
+
+
+
+            </div>
+
+            <div class="col-lg-4">
+qwedwed
+            </div>
+
+
+
+        </div>
+
+
+
+
+    </div>
 
 
 
